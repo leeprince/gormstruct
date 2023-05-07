de181d9a288ee38c9f17e9d26796ec49690e3004
leeprince <leeprince@foxmail.com>
2023-05-08 00:55:32 +0800

    feature: ioutil.ReadFile > os.ReadFile

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/leeprince/gormstruct
 
-go 1.16
+go 1.18
 
 require (
 	github.com/leeprince/goinfra v0.0.0-20220724025735-f1bc2ba63aff
@@ -11,4 +11,28 @@ require (
 	gopkg.in/yaml.v3 v3.0.1
 	gorm.io/driver/mysql v1.3.4
 	gorm.io/gorm v1.23.5
+)
+
+require (
+	github.com/axgle/mahonia v0.0.0-20180208002826-3358181d7394 // indirect
+	github.com/getsentry/sentry-go v0.13.0 // indirect
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/gookit/color v1.5.1 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.4 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
+	github.com/lestrrat-go/strftime v1.0.5 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.15.0 // indirect
+	golang.org/x/sys v0.0.0-20211216021012-1d35b9e2eb4e // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
 )
--- internal/config/init.go
+++ internal/config/init.go
@@ -1,11 +1,11 @@
 package config
 
 import (
-    "github.com/leeprince/gopublic/tools"
-    "github.com/leeprince/gormstruct/internal/logger"
-    "gopkg.in/yaml.v3"
-    "io/ioutil"
-    "path"
+	"github.com/leeprince/gopublic/tools"
+	"github.com/leeprince/gormstruct/internal/logger"
+	"gopkg.in/yaml.v3"
+	"os"
+	"path"
 )
 
 /**
@@ -16,38 +16,38 @@ import (
 
 // 引入文件初始化
 func InitConfig() {
-    once.Do(func() {
-        configPath = path.Join(tools.GetModelPath(), "config/config.yaml")
-        logger.Info("configPath:", configPath)
-        if !tools.CheckFileIsExist(configPath) {
-            logger.Panic("config.yaml not exit. using default config")
-        }
-        
-        onInit()
-    })
+	once.Do(func() {
+		configPath = path.Join(tools.GetModelPath(), "config/config.yaml")
+		logger.Info("configPath:", configPath)
+		if !tools.CheckFileIsExist(configPath) {
+			logger.Panic("config.yaml not exit. using default config")
+		}
+		
+		onInit()
+	})
 }
 
 // 初始化
 func onInit() {
-    err := initConfigFile(configPath)
-    if err != nil {
-        // fmt.Println("Load config file error: ", err.Error())
-        return
-    }
+	err := initConfigFile(configPath)
+	if err != nil {
+		// fmt.Println("Load config file error: ", err.Error())
+		return
+	}
 }
 
 // 初始化配置文件
 func initConfigFile(filename string) error {
-    bs, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return err
-    }
-    
-    if err := yaml.Unmarshal(bs, &config); err != nil {
-        // fmt.Println("read config file error: ", err.Error())
-        return err
-    }
-    
-    logger.Info("Config:", config)
-    return nil
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+	
+	if err := yaml.Unmarshal(bs, &config); err != nil {
+		// fmt.Println("read config file error: ", err.Error())
+		return err
+	}
+	
+	logger.Info("Config:", config)
+	return nil
 }
